1074ee34f70569d44e1b91c3c9746e4e39a2a4f1
zwy123zwy <81016312+zwy123zwy@users.noreply.github.com>
2024-02-13 12:31:30 +0800

    第六次提交

---
+++ Code_Generator_Web_Frontend/src/pages/User/Register/index.tsx
@@ -0,0 +1,156 @@
+import { userRegisterUsingPost } from '@/services/backend/userController';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { LoginForm, ProFormText } from '@ant-design/pro-components';
+import { useEmotionCss } from '@ant-design/use-emotion-css';
+import { Helmet, history, useModel,Link } from '@umijs/max';
+import { message, Tabs } from 'antd';
+import React, { useState } from 'react';
+import Settings from '../../../../config/defaultSettings';
+// React.FC React.FC是一个函数式组件，是在TypeScript使用一个泛型，FC就是FunctionComponent的缩写，
+// 事实上React.FC可以写成React.FunctionComponent
+/**
+ * 用户注册页面
+ * @returns 
+ */
+const Register: React.FC = () => {
+  const [type, setType] = useState<string>('account');
+  const { initialState, setInitialState } = useModel('@@initialState');
+  const containerClassName = useEmotionCss(() => {
+    return {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      overflow: 'auto',
+      backgroundImage:
+        "url('https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/V-_oS6r-i7wAAAAAAAAAAAAAFl94AQBr')",
+      backgroundSize: '100% 100%',
+    };
+  });
+
+  const handleSubmit = async (values: API.UserRegisterRequest) => {
+    try {
+      // 登录
+      const res = await userRegisterUsingPost({
+        ...values,
+      });
+
+      const defaultLoginSuccessMessage = '注册成功！';
+      message.success(defaultLoginSuccessMessage);
+      // 返回登陆页面
+      history.push("/user/login");
+      return;
+    } catch (error: any) {
+      const defaultLoginFailureMessage = `注册失败，${error.message}`;
+      message.error(defaultLoginFailureMessage);
+    }
+  };
+
+  return (
+    <div className={containerClassName}>
+      {/* 页面标签信息展示 */}
+      <Helmet>
+        <title>
+          {'注册'}- {Settings.title}
+        </title>
+      </Helmet>
+      <div
+        style={{
+          flex: '1',
+          padding: '32px 0',
+        }}
+      >
+        <LoginForm
+          contentStyle={{
+            minWidth: 280,
+            maxWidth: '75vw',
+          }}
+          logo={<img alt="logo" style={{ height: '100%' }} src="/LOGO1.svg" />}
+          title="EasyCode生成平台"
+          subTitle={'一个简单、好用的代码生成平台'}
+          initialValues={{
+            autoLogin: true,
+          }}
+          // {/* 高级组件修改内部的按钮文字 */}
+          submitter={{
+            searchConfig: {
+              submitText:"注册"
+            },
+          }}
+          onFinish={async (values) => {
+            await handleSubmit(values as API.UserRegisterRequest);
+          }}
+        >
+        
+          <Tabs
+            activeKey={type}
+            onChange={setType}
+            centered
+            items={[
+              {
+                key: 'account',
+                label: '新用户注册',
+              },
+            ]}
+          />
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="userAccount"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined />,
+                }}
+                placeholder={'请输入账号'}
+                rules={[
+                  {
+                    required: true,
+                    message: '账号是必填项！',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="userPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'请输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '密码是必填项！',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="checkPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'请再次输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '确认密码是必填项！',
+                  },
+                ]}
+              />
+            </>
+          )}
+
+          <div
+            style={{
+              marginBottom: 24,
+              textAlign: 'right',
+            }}
+          >
+            <Link to="/user/login" style={{color:'skyblue'}}>用户登录</Link>
+          </div>
+        </LoginForm>
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+export default Register;
